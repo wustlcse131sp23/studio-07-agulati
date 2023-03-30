c22246f9506622b2a68c08a62cf6868e575593a5
armaangulati <armaangulati@localhost>
2023-03-30 14:12:49 -0500

    this is die stud 7

---
+++ src/studio7/dieclass.java
@@ -0,0 +1,18 @@
+package studio7;
+import java.util.Scanner;
+import java.util.Random;
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.Rectangle;
+
+public class dieclass {
+	private int fogiatto;
+	private Random rand;
+	
+	public die(int fogiatto) {
+		this.fogiatto = fogiatto;
+		this.rand = new Random();
+	}
+	
+	public 
+}
